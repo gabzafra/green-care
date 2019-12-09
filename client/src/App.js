3156307c410a850099ebd6f4ddde6fec62cafd69
gabzafra <gab.zafra@gmail.com>
2019-12-09 15:20:21 +0100

    fixed dotenv problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import './App.css';
+//import TodoService from './services/TodoService';
+//import TodoList from './components/TodoList/TodoList';
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import SignUp from './components/Signup/Signup';
+import AuthService from './services/AuthService';
+//import PrivateRoute from './guards/PrivateRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Wololoolo</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.todoService = new TodoService();
+    this.authService = new AuthService();
+  }
+
+  state = {
+    user: null
+  }
+
+  setUser = (user) => {
+    this.setState({ ...this.state, user })
+  }
+
+  fetchUser = () => {
+    if (this.state.user === null) {
+      this.authService.loggedInUser()
+        .then(
+          (user) => {
+            this.setUser(user)
+          },
+          (error) => {
+            this.setUser(false)
+          }
+        )
+        .catch(() => {
+          this.setUser(false)
+        })
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUser()
+  }
+
+  render() {
+    this.fetchUser()
+    const { user } = this.state;
+    console.log(process.env)
+    return (
+      <div className="App">
+        <header className="App-header">
+          {user && <Switch>
+            <Route exact path="/login" render={(match) => <Login {...match} setUser={this.setUser} />} />  
+            <Route exact path="/signup" render={(match) => <SignUp {...match} setUser={this.setUser} />} />
+            {/* <PrivateRoute exact path="/" user={user} component={TodoList} /> */}
+          </Switch> }
+          {!user && <Switch>
+            <Route exact path="/login" render={(match) => <Login {...match} setUser={this.setUser} />} />  
+            <Route exact path="/signup" render={(match) => <SignUp {...match} setUser={this.setUser} />} />
+            {/* <PrivateRoute exact path="/" user={user} component={TodoList} /> */}
+          </Switch> }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
