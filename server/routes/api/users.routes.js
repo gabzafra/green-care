0902b84480b436d48a36e2728cbc30ba8dea8d31
gabzafra <gab.zafra@gmail.com>
2019-12-19 10:05:06 +0100

    delete plants ftw

--- server/routes/api/users.routes.js
+++ server/routes/api/users.routes.js
@@ -14,7 +14,9 @@ router.get("/deep/:id", (req, res, next) => {
         model: "Task"
       }
     })
-    .then(foundUser => res.status(200).json(foundUser))
+    .then(foundUser => {
+      res.status(200).json(foundUser);
+    })
     .catch(err => res.status(500).json(err));
 });
 
@@ -34,10 +36,19 @@ router.get("/shallow/:id", (req, res, next) => {
 
 router.put("/update", (req, res, next) => {
   const { id } = req.body;
+  console.log(req.body.plants);
   User.findByIdAndUpdate(id, req.body)
     .then(() => res.status(200).json({ message : "User updated"}))
     .catch(err => res.status(500).json(err));
 });
+
+router.delete("/deletePlant", (req, res, next) => {
+  const { user, plant } = req.body;
+  User.findByIdAndUpdate(user.id, {$pull: { plants: plant.id }})
+    .then(() => res.status(200).json({ message : "User plant deleted"}))
+    .catch(err => res.status(500).json(err));
+});
+
 
 router.post('/upload', uploader.single('picture'), (req, res) => {
   if(req.file){
@@ -46,5 +57,7 @@ router.post('/upload', uploader.single('picture'), (req, res) => {
     res.status(500).json({ message: 'Something went wrong' });
   }
 })
+
+
 
 module.exports = router;
