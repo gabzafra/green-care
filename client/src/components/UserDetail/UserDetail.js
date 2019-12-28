aea25ff1cdf4066fba543628a48b3888e5716f38
gabzafra <gab.zafra@gmail.com>
2019-12-28 16:24:42 +0100

    added pageTitle with controls to user-detail

--- client/src/components/UserDetail/UserDetail.js
+++ client/src/components/UserDetail/UserDetail.js
@@ -4,6 +4,7 @@ import ModalButtons from "../../sharedComponents/ModalButtons";
 import userService from "../../services/UserService";
 import capitalize from "../../globalStyles/utils";
 import LoadingOverlay from "../../sharedComponents/LoadingOverlay";
+import PageTitle from "../../sharedComponents/PageTitle";
 import "./UserDetail.css";
 import { Link } from "react-router-dom";
 
@@ -35,8 +36,6 @@ export default class UserDetail extends Component {
       });
   }
 
-  
-
   render() {
     const { name, email, picture, plants } = this.state;
     const userId = this.props.location.state.id;
@@ -46,40 +45,40 @@ export default class UserDetail extends Component {
           <LoadingOverlay />
         ) : (
           plants && (
-            <div className="big-div">
-              <h1>{capitalize(name)}</h1>
-              <ImageLoader
-                picture={picture}
-                handleUpload={() => ""}
-                flavour="readonly"
-              />
-              <h2 className="email-row">{email}</h2>
-              <div className="small-div">
-                {plants.splice(0, 3).map((plant, idx) => (
-                  <Link
-                  key={idx}
-                    to={{
-                      pathname: `/plant-detail/${plant.id}`,
-                      state: {
-                        flavour: "readonly",
-                        sourceUserId: `${userId}`
-                      }
-                    }}
-                  >
-                    <ImageLoader               
-                      picture={plant.picture}
-                      handleUpload={() => ""}
-                      flavour="readonly"
-                      size="120"
-                    />
-                  </Link>
-                ))}
+            <React.Fragment>
+              <PageTitle logoutHandler={this.props.handleLogout} controls={true}/>
+              <div className="big-div">
+                <h1>{capitalize(name)}</h1>
+                <ImageLoader
+                  picture={picture}
+                  handleUpload={() => ""}
+                  flavour="readonly"
+                />
+                <h2 className="email-row">{email}</h2>
+                <div className="small-div">
+                  {plants.splice(0, 3).map((plant, idx) => (
+                    <Link
+                      key={idx}
+                      to={{
+                        pathname: `/plant-detail/${plant.id}`,
+                        state: {
+                          flavour: "readonly",
+                          sourceUserId: `${userId}`
+                        }
+                      }}
+                    >
+                      <ImageLoader
+                        picture={plant.picture}
+                        handleUpload={() => ""}
+                        flavour="readonly"
+                        size="120"
+                      />
+                    </Link>
+                  ))}
+                </div>
+                <ModalButtons updateHandler={() => ""} flavour="readonly" />
               </div>
-              <ModalButtons
-                updateHandler={() => ""}
-                flavour="readonly"
-              />
-            </div>
+            </React.Fragment>
           )
         )}
       </React.Fragment>
