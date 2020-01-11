8b63d547b54c0a1336f9f6971c463a3099188a90
gabzafra <gab.zafra@gmail.com>
2020-01-11 09:32:18 +0100

    calendar day component styling

--- client/src/components/CalendarDay/CalendarDay.js
+++ client/src/components/CalendarDay/CalendarDay.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./CalendarDay.css";
 
 export default class CalendarDay extends Component {
-    render() {
-        return (
-            <div className="day-container">
-                <span className="day-slug">{this.props.daySlug}</span>
-                <img className="weather-icon" src={this.props.weatherIcon} alt=""/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="day-container">
+      <div className="task-wrapper">
+        <span className="day-slug">{this.props.daySlug}</span>
+        <div className="links-wrapper">
+          <div className="task-pic task-pic-blue"></div>
+          <div className="task-pic task-pic-red"></div>
+          <div className="task-pic task-pic-blue"></div>
+          <div className="task-pic task-pic-blue"></div>
+          <div className="task-pic task-pic-red"></div>
+          <div className="task-pic task-pic-blue"></div>
+        </div>  
+      </div>  
+        <img className="weather-icon" src={this.props.weatherIcon} alt="" />
+      </div>
+    );
+  }
 }
